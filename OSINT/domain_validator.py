130b01a516dfd667e30e8b7cd832246b1815e552
GraysonMor31 <grayson.r.morris@gmail.com>
2025-01-31 09:08:34 -0700

    Adding domain validator

---
+++ OSINT/domain_validator.py
@@ -0,0 +1,25 @@
+import whois
+
+def is_registered(domain):
+    """
+    Use a boolean to check if a domain is registered or not
+    
+    Parameters: 
+        domain (string)
+        
+    Returns: 
+        boolean: true/false
+        
+    example:
+        is_registered("google.com")
+    """
+    try:
+        who = whois.whois(domain)
+    except Exception:
+        return False
+    else:
+        return bool(who.domain_name)
+    
+def main():
+    domain = input("Enter a domain: ")
+    print(is_registered(domain))
