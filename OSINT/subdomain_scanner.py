ae6f7931a23110e48fd2069100646702a385c9fa
GraysonMor31 <grayson.r.morris@gmail.com>
2025-03-19 16:05:46 -0600

    added more subdomains

--- OSINT/subdomain_scanner.py
+++ OSINT/subdomain_scanner.py
@@ -15,7 +15,7 @@ def subdomain_scanner(domain, timeout=3):
             print("[+] Discovered subdomain: ", url)
             discovered_subdomains.append(url)
             
-    #Save discovered subdomains to file
+    #Save discovered subdomains to file, if doesn't exist create it
     with open(f"./TXT_FILES/{domain}_subdomains.txt", "w") as file:
         for subdomain in discovered_subdomains:
             file.write(subdomain + "\n")
